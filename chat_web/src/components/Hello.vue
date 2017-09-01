73c5e3f0e7a796cff11a3ed594b767fe42e6132d
xun li <xun li>
2017-09-01 09:49:53 +0800

    web项目提交

---
+++ chat_web/src/components/Hello.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+     用户名:<input type="text" placeholder="请输入用户名" name="userName"/><br/>
+     密&nbsp;码<input type="password" placeholder="请输入密码" name="password"/><br/>
+     <input type="submit" value="提交"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
